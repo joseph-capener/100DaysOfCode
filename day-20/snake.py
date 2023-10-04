0a5f421d0df92025bc766e0f32cb0675d31c233b
Joseph Capener <44791086+joseph-capener@users.noreply.github.com>
2023-10-04 11:57:07 -0600

    Snake moves better, updates properly

--- day-20/snake.py
+++ day-20/snake.py
@@ -1,52 +1,81 @@
+import time
 from turtle import Turtle, Screen
 
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 SNAKE_WIDTH = 1
-DISTANCE = 12
+DISTANCE = 20
 ROTATION = 90
 
-
-snake = []
 screen = Screen()
-screen.setup(width=WINDOW_WIDTH,height=WINDOW_HEIGHT)
+screen.setup(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
 
-def add_tail(color) -> Turtle:
+def add_tail(color, pos) -> Turtle:
     new_tail = Turtle()
     new_tail.penup()
     new_tail.shape("square")
-    new_tail.shapesize(SNAKE_WIDTH)
+    new_tail.turtlesize(stretch_wid=SNAKE_WIDTH, stretch_len=SNAKE_WIDTH)
     new_tail.color(color)
-    new_tail.speed(1)
+    new_tail.speed('fastest')
+    new_tail.setpos(pos)
     return new_tail
 
 
-def counter_clockwise_turn() -> None:
-    snake[0].left(angle=ROTATION)
+def north() -> None:
+    if snake[0].heading() != 270.0:
+        snake[0].setheading(90.0)
 
 
-def clockwise_turn() -> None:
-    snake[0].right(angle=ROTATION)
+def south() -> None:
+    if snake[0].heading() != 90.0:
+        snake[0].setheading(270.0)
+
+
+def east() -> None:
+    if snake[0].heading() != 180.0:
+        snake[0].setheading(0.0)
+
+
+def west() -> None:
+    if snake[0].heading() != 0.0:
+        snake[0].setheading(180.0)
 
 
 screen.listen()
-screen.onkey(key="a", fun=counter_clockwise_turn)
-screen.onkey(key="d", fun=clockwise_turn)
 
-# Create initial snake
-snake.append(add_tail('black'))
-snake.append(add_tail('blue'))
+screen.onkey(key="a", fun=west)
+screen.onkey(key="d", fun=east)
+screen.onkey(key="w", fun=north)
+screen.onkey(key="s", fun=south)
 
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+snake = []
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    snake.append(new_segment)
+    screen.update()
 game_over = False
 while not game_over:
-    coords = snake[0].pos()
+    # coords = snake[0].pos()
+    # snake[0].forward(DISTANCE)
+    # snake[-1].setpos(coords)
+    # snake.append(add_tail(color='white', pos=coords))
+
+    for seg_num in range(len(snake)-1, 0, -1):
+        coords = snake[seg_num - 1].pos()
+        snake[seg_num].goto(coords)
     snake[0].forward(DISTANCE)
-    snake[1].setpos(coords)
-
-
-    if abs(snake[0].xcor()) > WINDOW_WIDTH/2 or abs(snake[0].ycor()) > WINDOW_HEIGHT/2:
+    if abs(snake[0].xcor()) > WINDOW_WIDTH / 2 or abs(snake[0].ycor()) > WINDOW_HEIGHT / 2:
         game_over = True
-
+    screen.update()
+    time.sleep(0.1)
 
 screen.exitonclick()
